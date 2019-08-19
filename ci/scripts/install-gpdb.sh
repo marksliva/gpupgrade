9acad8d00fbb69ce04cddf38c2341805c31c6664
Mark Sliva <msliva@pivotal.io>
2019-08-19 10:56:26 -0600

    Revert "wip - temporarily use the github rpm"
    This reverts commit 5b688f09096cf12334e038601f7a2f3026763bd2.

--- ci/scripts/install-gpdb.sh
+++ ci/scripts/install-gpdb.sh
@@ -7,15 +7,11 @@ install_gpdb_rpm() {
     scp "${rpm_dir}"/*.rpm "${node_hostname}:/tmp/gpdb_new.rpm"
     ssh -ttn centos@"$node_hostname" '
         sudo rpm -hi /tmp/gpdb_new.rpm
-        sudo chown -R gpadmin:gpadmin /usr/local/greenplum-database*
+        sudo chown -R gpadmin:gpadmin /usr/local/greenplum-db*
     '
 }
 
 ./ccp_src/scripts/setup_ssh_to_cluster.sh
-
-# todo: temporarily use the github rpm
-rm $RPM_DIR/*
-wget https://github.com/greenplum-db/gpdb/releases/download/6.0.0-beta.6/greenplum-database-6.0.0-beta.6-rhel6-x86_64.rpm -P $RPM_DIR
 
 for segment_host in $(cat cluster_env_files/hostfile_all); do
     install_gpdb_rpm $segment_host "${RPM_DIR}"
